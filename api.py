c678988f827d55096e00577358766581ecd02af9
Antonio Vázquez Gutiérrez <avazquez62@alumnos.uaq.mx>
2020-10-18 19:37:58 -0500

    API and DB implementation
    MONGODB ATLAS

--- api.py
+++ api.py
@@ -1,17 +1,46 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from researchGate import findResearchGate
 from googleAcademic import findGoogle
+from database import queryDatabase, insertData
+from bson import json_util
 
 app = Flask(__name__)
 
+# Consulta la base de datos, de no tener datos se hace el scrape
 @app.route('/<string:name>')
 def query_by_name(name):
+
+    queryResult = queryDatabase(name)
+    if(queryResult != None):
+        return queryResult
+    else:
+        scrapeResults = {
+            'research_gate' : findResearchGate(name),
+            'google' : findGoogle(name)
+        }
+
+        insertedData = insertData(name, scrapeResults)
+        
+        return insertedData
+
+# Consulta únicamente la base de datos
+@app.route('/db/<string:name>')
+def query(name):
+    results = queryDatabase(name)
+    if(results != None):
+        print (results)
+        return results
+    else:
+        return { 'error' : 'Sin resultados' }
+
+# Realiza únicamente el scrape, sin guardar los resultados 
+@app.route('/scrape/<string:name>')
+def scrape(name):
 
     return {
         "research_gate" : findResearchGate(name),
         "google" : findGoogle(name)
     }
-
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,24 @@
+import datetime
+from pymongo import MongoClient
+connectionString="mongodb+srv://admin:admin@cluster0.snt3m.mongodb.net/<dbname>?retryWrites=true&w=majority"
+# localConnection=""
+client = MongoClient(connectionString)
+db=client.scraped
+authors = db.authors
+
+def queryDatabase(search_param):
+    result = authors.find_one({ 'full_name' : search_param }, {'_id':False} )
+    if(result is not None):
+        return result
+    else: 
+        return None
+
+def insertData(name, data):
+
+    return authors.replace_one({ 'full_name' : name }, {
+        'name' : name,
+        'date' : datetime.datetime.now(),
+        'research_gate' : data.research_gate,
+        'google' : data.google
+
+    })
